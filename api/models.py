eae50b9f35a06535672c08ccc9a032b3dca1a71b
SyedMaazHassan <hafizmaazhassan33@gmail.com>
2022-04-06 15:23:16 +0500

    Drive link addeD

--- api/models.py
+++ api/models.py
@@ -19,21 +19,24 @@ import json
 # python manage.py runserver
 
 # class Tag(mode)
+
+
 class SystemUser(models.Model):
-    uid = models.CharField(unique = True, max_length = 255)
-    stripe_cust_id = models.CharField(max_length = 25)
-    avatar = models.ImageField(upload_to = "avatars", null = True, blank = True, default = 'avatars/default-profile.png')
-    display_name = models.CharField(max_length = 255, null = True, blank = True)
-    first_name = models.CharField(max_length = 255)
-    last_name = models.CharField(max_length = 255)
-    email = models.EmailField(unique = True)
-    phone = models.CharField(max_length = 15, unique = True, blank = True, null = True)
-    about = models.TextField(null = True, blank = True)
-    is_trial_synced = models.BooleanField(default = True)
-    created_at = models.DateTimeField(default = timezone.now)
-    is_trial_taken = models.BooleanField(default = False)
-    is_trial_end = models.BooleanField(default = False)
-    is_fee_paid = models.BooleanField(default = False)
+    uid = models.CharField(unique=True, max_length=255)
+    stripe_cust_id = models.CharField(max_length=25)
+    avatar = models.ImageField(
+        upload_to="avatars", null=True, blank=True, default='avatars/default-profile.png')
+    display_name = models.CharField(max_length=255, null=True, blank=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, unique=True, blank=True, null=True)
+    about = models.TextField(null=True, blank=True)
+    is_trial_synced = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    is_trial_taken = models.BooleanField(default=False)
+    is_trial_end = models.BooleanField(default=False)
+    is_fee_paid = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.first_name} - {self.uid}"
@@ -43,7 +46,7 @@ class SystemUser(models.Model):
         verbose_name_plural = 'Users'
 
 
-@receiver(post_save, sender = SystemUser)
+@receiver(post_save, sender=SystemUser)
 def unlock_all_missions(sender, instance, **kwargs):
     user = instance
     if kwargs['created']:
@@ -52,11 +55,12 @@ def unlock_all_missions(sender, instance, **kwargs):
         for course in all_courses:
             unlock_first_level_and_mission(user, course)
 
+
 class Category(models.Model):
     cat_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length = 15)
-    icon = models.CharField(max_length = 15)
-    created_at = models.DateTimeField(default = timezone.now)
+    name = models.CharField(max_length=15)
+    icon = models.CharField(max_length=15)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def get_next_cat(self):
         all_cats = Category.objects.all()
@@ -73,21 +77,23 @@ class Category(models.Model):
     class Meta:
         ordering = ('cat_id',)
 
+
 class Course(models.Model):
     course_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length = 15)
-    category = models.ForeignKey(Category, related_name='courses', on_delete = models.CASCADE)
-    available_on_free_trial = models.BooleanField(default = False)
-    created_at = models.DateTimeField(default = timezone.now)
+    name = models.CharField(max_length=15)
+    category = models.ForeignKey(
+        Category, related_name='courses', on_delete=models.CASCADE)
+    available_on_free_trial = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def get_next_course(self):
-        all_courses = Course.objects.filter(category = self.category)
+        all_courses = Course.objects.filter(category=self.category)
         length = all_courses.count()
         for index in range(length):
             next_index = index + 1
             if all_courses[index] == self and (next_index < length):
                 return all_courses[next_index]
-        return None  
+        return None
 
     def __str__(self):
         return f"{self.category} / {self.name} ({self.course_id})"
@@ -96,24 +102,27 @@ class Course(models.Model):
         ordering = ('course_id',)
 
 
-@receiver(post_save, sender = Course)
+@receiver(post_save, sender=Course)
 def unlock_demo_for_all_users(sender, instance, **kwargs):
     if not kwargs['created']:
         all_users = SystemUser.objects.all()
         new_course = instance
         for user in all_users:
             unlock_first_level_and_mission(user, new_course)
-    
+
+
 class Level(models.Model):
     level_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length = 15)
-    tagline = models.CharField(max_length = 70)
-    display_pic = models.ImageField(upload_to = 'levels', default = 'levels/default-display.jpg')
-    course = models.ForeignKey(Course, related_name='levels', on_delete = models.CASCADE)
-    created_at = models.DateTimeField(default = timezone.now)
+    name = models.CharField(max_length=15)
+    tagline = models.CharField(max_length=70)
+    display_pic = models.ImageField(
+        upload_to='levels', default='levels/default-display.jpg')
+    course = models.ForeignKey(
+        Course, related_name='levels', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def get_next_level(self):
-        all_levels = Level.objects.filter(course = self.course)
+        all_levels = Level.objects.filter(course=self.course)
         length = all_levels.count()
         for index in range(length):
             next_index = index + 1
@@ -130,13 +139,15 @@ class Level(models.Model):
 
 class Mission(models.Model):
     mission_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length = 15)
-    display_pic = models.ImageField(upload_to = 'missions', default = 'missions/default-display.jpg')
-    level = models.ForeignKey(Level, related_name='missions', on_delete = models.CASCADE)
-    created_at = models.DateTimeField(default = timezone.now)
+    name = models.CharField(max_length=15)
+    display_pic = models.ImageField(
+        upload_to='missions', default='missions/default-display.jpg')
+    level = models.ForeignKey(
+        Level, related_name='missions', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def get_next_mission(self):
-        all_missions = Mission.objects.filter(level = self.level)
+        all_missions = Mission.objects.filter(level=self.level)
         length = all_missions.count()
         for index in range(length):
             next_index = index + 1
@@ -146,21 +157,22 @@ class Mission(models.Model):
 
     def get_length(self, filename):
         data = cv2.VideoCapture(filename)
-  
+
         # count the number of frames
         frames = data.get(cv2.CAP_PROP_FRAME_COUNT)
         fps = int(data.get(cv2.CAP_PROP_FPS))
-        
+
         # calculate dusration of the video
         seconds = int(frames / fps)
         video_time = str(timedelta(seconds=seconds))
         return video_time
 
     def duration(self):
-        query = Video.objects.filter(mission = self)
+        query = Video.objects.filter(mission=self)
         if query.exists():
             video = query[0]
-            video_path = os.path.join(settings.BASE_DIR, 'media', str(video.video_url))
+            video_path = os.path.join(
+                settings.BASE_DIR, 'media', str(video.video_url))
 
             return self.get_length(video_path)
         return None
@@ -178,13 +190,16 @@ class Mission(models.Model):
     class Meta:
         ordering = ('mission_id',)
 
+
 class Video(models.Model):
     video_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length = 15)
-    description = models.CharField(max_length = 50)
-    mission = models.OneToOneField(Mission, related_name='video', on_delete = models.CASCADE)
-    video_url = models.FileField(upload_to = 'videos')
-    created_at = models.DateTimeField(default = timezone.now)
+    title = models.CharField(max_length=15)
+    description = models.CharField(max_length=50)
+    mission = models.OneToOneField(
+        Mission, related_name='video', on_delete=models.CASCADE)
+    video_url = models.URLField(
+        default='https://drive.google.com/file/d/1qpvYebZHyEW1LBLJ8I8t7-ZnkfFi7A0A/view?usp=sharing')
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"{self.mission} / {self.title}"
@@ -194,25 +209,26 @@ class Video(models.Model):
 
 
 class Currency(models.Model):
-    code = models.CharField(max_length = 6)
-    name = models.CharField(max_length = 20)
-    symbol = models.CharField(max_length = 10)
+    code = models.CharField(max_length=6)
+    name = models.CharField(max_length=20)
+    symbol = models.CharField(max_length=10)
 
     def __str__(self):
         return f'{self.code}'
 
+
 class Subscription(models.Model):
     subs_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length = 30)
-    subtitle = models.CharField(max_length = 30)
+    title = models.CharField(max_length=30)
+    subtitle = models.CharField(max_length=30)
     price = models.FloatField()
-    currency = models.ForeignKey(Currency, on_delete = models.CASCADE)
-    sales_tax = models.IntegerField(default = 9)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    sales_tax = models.IntegerField(default=9)
     freq = models.CharField(
-            max_length = 20, 
-            choices=[('month', 'monthly'), ('year', 'yearly'), ('week', 'weekly')])
+        max_length=20,
+        choices=[('month', 'monthly'), ('year', 'yearly'), ('week', 'weekly')])
     ft_days = models.IntegerField()
-    is_best = models.BooleanField(default = False)
+    is_best = models.BooleanField(default=False)
 
     def sales_tax_price(self):
         return round((self.price / 100) * self.sales_tax, 2)
@@ -227,7 +243,7 @@ class Subscription(models.Model):
 
     def save(self, *args, **kwargs):
         if self.is_best:
-            Subscription.objects.all().update(is_best = False)
+            Subscription.objects.all().update(is_best=False)
             self.is_best = True
         super(Subscription, self).save(*args, **kwargs)
 
@@ -237,10 +253,11 @@ class Subscription(models.Model):
 
 class Trial(models.Model):
     id = models.AutoField(primary_key=True)
-    subscription = models.ForeignKey(Subscription, on_delete = models.CASCADE)
-    started_at = models.DateTimeField(default = timezone.now)
-    is_active = models.BooleanField(default = True)
-    user = models.ForeignKey(SystemUser, on_delete = models.CASCADE, null = True, blank = True)
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+    started_at = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    user = models.ForeignKey(
+        SystemUser, on_delete=models.CASCADE, null=True, blank=True)
 
     def end_at(self):
         return self.started_at + timedelta(days=self.subscription.ft_days)
@@ -257,15 +274,16 @@ class Trial(models.Model):
     def __str__(self):
         return f'{self.subscription} by User: {self.user}'
 
+
 class Payment(models.Model):
-    pay_id = models.AutoField(primary_key = True)
-    subscription = models.ForeignKey(Subscription, on_delete = models.CASCADE)
-    user = models.ForeignKey(SystemUser, on_delete = models.CASCADE)
-    ephemeral_key = models.CharField(max_length = 60)
-    payment_intent = models.CharField(max_length = 60)
-    status = models.IntegerField(default = 0)
-    created_at = models.DateTimeField(default = timezone.now)
-    updated_at = models.DateTimeField(default = timezone.now)
+    pay_id = models.AutoField(primary_key=True)
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+    user = models.ForeignKey(SystemUser, on_delete=models.CASCADE)
+    ephemeral_key = models.CharField(max_length=60)
+    payment_intent = models.CharField(max_length=60)
+    status = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
 
     def end_at(self):
         days = 0
@@ -286,18 +304,22 @@ class Payment(models.Model):
     class Meta:
         ordering = ('pay_id',)
 
+
 class UnlockedLevel(models.Model):
-    level = models.ForeignKey(Level, on_delete = models.CASCADE)
-    is_completed = models.BooleanField(default = False)
-    user = models.ForeignKey(SystemUser, on_delete = models.CASCADE, null = True, blank = True)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False)
+    user = models.ForeignKey(
+        SystemUser, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f'{self.user} => {self.level} => Completed: {self.is_completed}'
 
+
 class UnlockedMission(models.Model):
-    mission = models.ForeignKey(Mission, on_delete = models.CASCADE)
-    is_completed = models.BooleanField(default = False)
-    user = models.ForeignKey(SystemUser, on_delete = models.CASCADE, null = True, blank = True)
+    mission = models.ForeignKey(Mission, on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False)
+    user = models.ForeignKey(
+        SystemUser, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f'({self.pk}) {self.user} => {self.mission} => Completed: {self.is_completed}'
@@ -305,13 +327,16 @@ class UnlockedMission(models.Model):
     class Meta:
         ordering = ('pk',)
 
+
 class CompletedCourse(models.Model):
-    course = models.ForeignKey(Course, on_delete = models.CASCADE)
-    user = models.ForeignKey(SystemUser, on_delete = models.CASCADE, null = True, blank = True)
-    timestamp = models.DateTimeField(default = timezone.now)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        SystemUser, on_delete=models.CASCADE, null=True, blank=True)
+    timestamp = models.DateTimeField(default=timezone.now)
+
 
 class API_Key(models.Model):
-    key = models.UUIDField(unique = True, default=uuid.uuid4, editable = False)
+    key = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return str(self.key)
@@ -320,7 +345,9 @@ class API_Key(models.Model):
         verbose_name = 'API key'
         verbose_name_plural = 'API keys'
 
+
 class LastVisit(models.Model):
-    user = models.ForeignKey(SystemUser, on_delete = models.CASCADE)
-    mission = models.ForeignKey(Mission, on_delete = models.CASCADE, null = True, blank = True)
-    timestamp = models.DateTimeField(default = timezone.now)
+    user = models.ForeignKey(SystemUser, on_delete=models.CASCADE)
+    mission = models.ForeignKey(
+        Mission, on_delete=models.CASCADE, null=True, blank=True)
+    timestamp = models.DateTimeField(default=timezone.now)
